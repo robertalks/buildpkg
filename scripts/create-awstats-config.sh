be6120b6a25b3965146fcf4196001fa05b8d02fc
Robert Milasan <rmilasan@suse.com>
2015-08-21 13:21:55 +0200

    re-work scripts/create-awstats-config.sh scripts/update-awstats.sh scripts

--- scripts/create-awstats-config.sh
+++ scripts/create-awstats-config.sh
@@ -1,55 +1,56 @@
-#!/bin/bash
+#!/bin/sh -e
 #
 # awstats config generator
 # part of buildpkg script.
 
-set -e
-
-NAME=$(basename $0)
-DOMAIN=$1
+NAME="$(basename $0)"
+DOMAIN="$1"
 DATA=@@DATA@@
 MODEL=@@PREFIX@@/conf/awstats.model.conf
 CONFIGDIR=@@PREFIX@@/conf
 NEWCONFIG="${CONFIGDIR}/awstats.${DOMAIN}.conf"
 WWWUSER=www
-_UID=$(id -u 2>/dev/null)
+ID=$(id -u 2>/dev/null)
+
+_info() { echo "$NAME: $@"; }
+_error() { echo "$NAME: error: $@" >&2; }
 
 if [ $# -eq 0 ]; then
-   echo "${NAME}: error: missing host/domain name argument."
-   echo "${NAME}: usage: ${NAME} <hostname>"
-   exit 1
+	_error "expected domain name" >&2
+	_info "usage: $NAME <domain_name>"
+	exit 1
 fi
 
 if [ ! -d ${CONFIGDIR} ]; then
-   echo "${NAME}: error: ${CONFIGDIR} missing, awstats mignt not be properly installed."
-   exit 1
+	_error "${CONFIGDIR} missing, awstats might not be properly installed." >&2
+	exit 1
 fi
 
 if [ ! -e ${MODEL} ]; then
-   echo "${NAME}: error: ${MODEL} config model missing."
-   echo "${NAME}: error: can't generate new configuration if config model is missing."
-   exit 1
+	_error "${MODEL} config model missing."
+	_error "can't generate new configuration if config model is missing."
+	exit 1
 fi
 
-if [ $_UID -ne 0 ]; then
-   echo "${NAME}: error: config generator must be run as root user."
-   exit 1
+if [ $ID -ne 0 ]; then
+	_error "config generator must be run as root user."
+	exit 1
 fi
 
-if [ $(getent passwd ${WWWUSER} >/dev/null 2>&1; echo $?) -ne 0 ]; then
-   echo "${NAME}: error: ${WWWUSER} missing, please reinstall the web server and try again."
-   exit 1
+if ! getent passwd ${WWWUSER} >/dev/null 2>&1; then
+	_error "${WWWUSER} missing, please reinstall the web server and try again."
+	exit 1
 fi
 
 if [ ! -e /var/log/nginx/${DOMAIN}-access.log ]; then
-   echo "${NAME}: error: /var/log/nginx/${DOMAIN}-access.log access log file not found."
-   echo "${NAME}: error: make sure you have everything setup correctly and that the domain is correct."
-   exit 1
+	_error "/var/log/nginx/${DOMAIN}-access.log access log file not found."
+	_error "make sure you have everything setup correctly and that the domain is correct."
+	exit 1
 fi
 
 if [ -e ${NEWCONFIG} ]; then
-   echo "${NAME}: error: ${NEWCONFIG} already exists, we wont overwrite it."
-   exit 1
+	_error "${NEWCONFIG} already exists, we wont overwrite it."
+	exit 1
 fi
 
 [ -d ${DATA} ] || mkdir -p ${DATA} >/dev/null 2>&1
@@ -70,7 +71,7 @@ sed -i \
 
 chmod 0644 ${NEWCONFIG} >/dev/null 2>&1
 
-echo "${NAME}: info: ${NEWCONFIG} configuration file generated."
-echo "${NAME}: info: please run update-awstats.sh script to generate the first statistics for domain ${DOMAIN}."
+_info "info: ${NEWCONFIG} configuration file generated."
+_info "info: please run update-awstats.sh script to generate the first statistics for domain ${DOMAIN}."
 
 exit 0
--- scripts/update-awstats.sh
+++ scripts/update-awstats.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh -e
 #
 # awstats statistics updater script.
 # part of buildpkg script.
 
-set -e
-
+NAME="$(basename $0 2>/dev/null)"
 PREFIX="@@PREFIX@@"
 AWSTATS="${PREFIX}/wwwroot/cgi-bin/awstats.pl"
 DATA="@@DATA@@"
@@ -12,87 +11,93 @@ DOMAIN=$1
 DATE="$(date '+%m%Y')"
 USER=www
 
-msg_error()
-{
- local msg="$1"
- local name="$(basename $0 2>/dev/null)"
- local date="$(date '+%H:%M:%S %d/%m/%Y')"
- echo "${name}: (${date}): error: ${msg}"
-}
+msg_error() { echo "$NAME: error: $@" >&2; }
+msg_info() { echo "$NAME: info: $@"; }
 
-msg_info()
-{
- local msg="$1"
- local name="$(basename $0 2>/dev/null)"
- local date="$(date '+%H:%M:%S %d/%m/%Y')"
- echo "${name}: (${date}): info: ${msg}"
+usage() {
+	cat << EOF
+$NAME: update awstats statistics
+
+Usage: $NAME [OPTIONS] <domain_name>...
+
+        --help          Show help
+        --all           Updates all statistics for all available domains
+
+Example:
+      $NAME youdomain.com
+    or
+      $NAME --all
+
+EOF
 }
 
 update_per_domain()
 {
-  local domain=$1
-  local config="${PREFIX}/conf/awstats.${domain}.conf"
+	local domain="$1"
+	local config=""
 
-  if [ "${domain}" = "" ]; then
-     msg_error "missing domain."
-     exit 1
-  fi
+	if [ "${domain}" = "" ]; then
+		msg_error "missing domain."
+		exit 1
+	fi
 
-  if [ -e ${config} ]; then
-     msg_info "running web statistics update on domain ${domain} ..."
-     ${AWSTATS} -update -config=${domain} >/dev/null 2>&1
-     if [ $? -ne 0 ]; then
-        msg_error "failed to update web statistics for domain ${domain}."
-        exit 1
-     fi
-  else
-     msg_error "missing awstats domain ${domain} configuration file."
-     exit 1
-  fi
+	config="${PREFIX}/conf/awstats.${domain}.conf"
+	if [ -e ${config} ]; then
+		msg_info "running web statistics update on domain ${domain} ..."
+		${AWSTATS} -update -config=${domain} >/dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			msg_error "failed to update web statistics for domain ${domain}."
+			exit 1
+     		fi
+	else
+		msg_error "missing awstats domain ${domain} configuration file."
+		exit 1
+	fi
 
-  if [ -e ${DATA}/awstats${DATE}.${domain}.txt ]; then
-     if [ "$(stat -c %U ${DATA}/awstats${DATE}.${domain}.txt)" != "${USER}" ]; then
-         chown ${USER} ${DATA}/awstats${DATE}.${domain}.txt >/dev/null 2>&1
-     fi
-  fi
-  if [ -e ${DATA}/dnscachelastupdate.${domain}.txt ]; then
-     if [ "$(stat -c %U ${DATA}/dnscachelastupdate.${domain}.txt)" != "${USER}" ]; then
-         chown ${USER} ${DATA}/dnscachelastupdate.${domain}.txt >/dev/null 2>&1
-     fi
-  fi
-
+	if [ -e ${DATA}/awstats${DATE}.${domain}.txt ]; then
+		if [ "$(stat -c %U ${DATA}/awstats${DATE}.${domain}.txt)" != "${USER}" ]; then
+			chown ${USER} ${DATA}/awstats${DATE}.${domain}.txt >/dev/null 2>&1
+		fi
+	fi
+	if [ -e ${DATA}/dnscachelastupdate.${domain}.txt ]; then
+		if [ "$(stat -c %U ${DATA}/dnscachelastupdate.${domain}.txt)" != "${USER}" ]; then
+			chown ${USER} ${DATA}/dnscachelastupdate.${domain}.txt >/dev/null 2>&1
+		fi
+	fi
 }
 
 update_all()
 {
- local files="$(find ${PREFIX}/conf \( -name awstats.\*.conf \) -a \( ! -name awstats.model.conf \) -type f)"
- if [ "${files}" = "" ]; then
-    msg_info "nothing to update, not configuration files found."
-    exit 0
- else
-    for conf in ${files}; do
-	domain="$(grep "^SiteDomain" ${conf} 2>/dev/null | cut -f 2 -d\" 2>/dev/null)"
-        update_per_domain "${domain}"
-    done
- fi
+	local files="$(find ${PREFIX}/conf \( -name awstats.\*.conf \) -a \( ! -name awstats.model.conf \) -type f)"
+
+	if [ "${files}" = "" ]; then
+		msg_info "nothing to update, not configuration files found."
+		exit 0
+	else
+		for conf in ${files}; do
+			domain="$(grep "^SiteDomain" ${conf} 2>/dev/null | cut -f 2 -d\" 2>/dev/null)"
+			update_per_domain "${domain}"
+		done
+	fi
 }
 
 if [ $# -eq 0 ]; then
-   msg_error "missing arguments."
-   msg_info "usage: <script> <domain> (updates all statistics per given domain)"
-   msg_info "usage: <script> --all (updates all statistics for all available domains)"
-   exit 1
+	usage
+	msg_error "missing option or domain name."
+	exit 1
 fi
 
 if [ ! -x ${AWSTATS} ]; then
-   msg_error "awstats perl script missing."
-   exit 1
+	msg_error "awstats perl script missing."
+	exit 1
 fi
 
-if [ "${DOMAIN}" = "--all" ]; then
-   update_all
-else
-   update_per_domain ${DOMAIN}
+if [ "${DOMAIN}" = "--help" ]; then
+	usage
+elif [ "${DOMAIN}" = "--all" ]; then
+	update_all
+elif
+	update_per_domain ${DOMAIN}
 fi
 
 exit 0
